7ae41d222a3abb08eda327e846b91900d5d967fb
chat69-g <abrak3518@gmail.com>
2025-05-10 17:04:06 +0200

    Avtomatska posodobitev sob. 10. 05. 2025 17:04:06,13

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,54 +1,10 @@
 #include "Map.hpp"
-#include "TextureManager.hpp"
-#include <vector>
+#include <iostream>
 
 Map::Map() {
-    // Initialize tile textures
-    TextureManager& tm = TextureManager::getInstance();
-    for (int i = 0; i < 5; ++i) {
-        tileTextures.push_back(tm.get("tile" + std::to_string(i)));
-    }
-
-    // Simple map layout (0 = empty, 1-4 = different tiles)
-    tiles = {
-        {1,1,1,1,1,1,1,1,1,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,0,2,0,3,0,4,0,0,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1}
-    };
+    // Konstruktor
 }
 
-Map::~Map() {
-    // Textures are managed by TextureManager
-}
-
-void Map::Render(SDL_Renderer* renderer) {
-    const int tileSize = 64;
-    for (size_t y = 0; y < tiles.size(); ++y) {
-        for (size_t x = 0; x < tiles[y].size(); ++x) {
-            int tileType = tiles[y][x];
-            if (tileType > 0) {
-                SDL_Rect destRect = {
-                    static_cast<int>(x) * tileSize,
-                    static_cast<int>(y) * tileSize,
-                    tileSize,
-                    tileSize
-                };
-                SDL_RenderCopy(renderer, tileTextures[tileType-1], nullptr, &destRect);
-            }
-        }
-    }
-}
-
-bool Map::IsWall(int x, int y) const {
-    int tileX = x / 64;
-    int tileY = y / 64;
-    
-    if (tileY >= 0 && tileY < static_cast<int>(tiles.size())) {
-        if (tileX >= 0 && tileX < static_cast<int>(tiles[tileY].size())) {
-            return tiles[tileY][tileX] != 0;
-        }
-    }
-    return true;
+void Map::generate() {
+    std::cout << "Generating map..." << std::endl;
 }
