b0a9d3f4197f4cfdcf5752c722ab0787149b0b86
chat69-g <abrak3518@gmail.com>
2025-05-10 17:10:27 +0200

    Avtomatska posodobitev sob. 10. 05. 2025 17:10:27,78

--- inc/ScoreManager.hpp
+++ inc/ScoreManager.hpp
@@ -6,13 +6,12 @@
 #include <algorithm>
 
 class ScoreManager {
-private:
-    std::vector<int> scores;
-
-public:
-    void SaveToFile(const std::string& filename);
-    void addScore(int score);
-    void saveScores(const std::string& filename);
-    void loadScores(const std::string& filename);
-    void displayTopScores() const;
-};
+    private:
+    std::vector<std::pair<std::string, int>> scores; // Popravljena deklaracija
+    
+    public:
+        void addScore(const std::string& name, int time); // Posodobljena deklaracija
+        void saveScores(const std::string& filename);
+        void loadScores(const std::string& filename);
+        void displayTopScores() const;
+    };
--- src/ScoreManager.cpp
+++ src/ScoreManager.cpp
@@ -4,35 +4,43 @@
 #include <algorithm>
 
 void ScoreManager::addScore(const std::string& name, int time) {
-    scores.emplace_back(name, time);
-    std::sort(scores.begin(), scores.end(), [](auto& a, auto& b) {
-        return a.second < b.second;
+    scores.emplace_back(name, time); // Dodaj ime in čas kot par
+    std::sort(scores.begin(), scores.end(), [](const auto& a, const auto& b) {
+        return a.second < b.second; // Razvrsti po času (naraščajoče)
     });
     if (scores.size() > 5) {
-        scores.pop_back();
+        scores.pop_back(); // Ohranimo samo top 5 rezultatov
     }
 }
 
 void ScoreManager::saveScores(const std::string& filename) {
     std::ofstream file(filename);
+    if (!file) {
+        std::cerr << "Error: Could not open file for writing: " << filename << std::endl;
+        return;
+    }
     for (const auto& score : scores) {
-        file << score.first << " " << score.second << "\n";
+        file << score.first << " " << score.second << "\n"; // Zapiši ime in čas
     }
 }
 
 void ScoreManager::loadScores(const std::string& filename) {
     std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Error: Could not open file for reading: " << filename << std::endl;
+        return;
+    }
     scores.clear();
     std::string name;
     int time;
     while (file >> name >> time) {
-        scores.emplace_back(name, time);
+        scores.emplace_back(name, time); // Preberi ime in čas
     }
 }
 
 void ScoreManager::displayTopScores() const {
     std::cout << "Top 5 Scores:\n";
     for (const auto& score : scores) {
-        std::cout << score.first << ": " << score.second << " seconds\n";
+        std::cout << score.first << ": " << score.second << " seconds\n"; // Prikaži ime in čas
     }
 }
